7d3eff42dc2c5e51ff1d234507d1a5c241ee6a0f
vethaspace <vethaspace@gmail.com>
2021-02-21 19:54:11 -0500

    First commit

---
+++ src/test/java/edu/vethathiri/employeemanager/app/EmployeeManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vethathiri.employeemanager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
